91575207c78d2f6d71943a08af5eb43b7a81e25f
Abdalla Mohamed <46408571+IbeAbdullaGit@users.noreply.github.com>
2023-02-05 17:05:52 -0500

    More updates on the Nexus 2

---
+++ Stealth/Assets/NoCursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
